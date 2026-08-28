56cf95211515074f810b9644b756be8635936567
jaguar <jude.pontes15@gmail.com>
2026-08-28 14:52:03 -0400

    Add hint feedback for high/low/correct guesses

--- NumberGuess.java
+++ NumberGuess.java
@@ -8,14 +8,39 @@ public class NumberGuess {
         Scanner scanner = new Scanner(System.in);
 
         int hiddenNumber = (int) (Math.random() * 100) + 1;
+        int count = 0;
         int guess = 0;
 
         while (guess != hiddenNumber) {
             System.out.print("Enter your guess: ");
             guess = scanner.nextInt();
+            count++;
+
+            canedoGuess g = new canedoGuess(guess, count);
+            System.out.println(g.hint(hiddenNumber));
         }
 
-        System.out.println("You got it!");
+        System.out.println("Solved in " + count + " attempts.");
         scanner.close();
+    }
+
+    public static class canedoGuess {
+        int value;
+        int guessedNumber;
+
+        canedoGuess(int value, int guessedNumber) {
+            this.value = value;
+            this.guessedNumber = guessedNumber;
+        }
+
+        String hint(int hiddenNumber) {
+            if (value > hiddenNumber) {
+                return "Attempt " + guessedNumber + ": " + value + " is too high";
+            } else if (value < hiddenNumber) {
+                return "Attempt " + guessedNumber + ": " + value + " is too low";
+            } else {
+                return "Attempt " + guessedNumber + ": " + value + " is correct!";
+            }
+        }
     }
 }
